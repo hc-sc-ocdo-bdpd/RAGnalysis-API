5d67de3d0afb2c98e5d67b037f7be99002336c8f
Ubuntu <BLUO@BLUO.rzogni4mlg2utd2zwouju2c5qh.vx.internal.cloudapp.net>
2024-03-14 13:23:15 +0000

    Refactored code to use OOP, added error messages, added logging

--- function/function_app.py
+++ function/function_app.py
@@ -12,139 +12,153 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
 
 @app.route(route="llama")
 def route_llama(req: func.HttpRequest) -> func.HttpResponse:
-    return model(req, 'llama')
+    return rag(req, 'llama').generate()
 
 
 @app.route(route="mistral")
 def route_mistral(req: func.HttpRequest) -> func.HttpResponse:
-    return model(req, 'mistral')
+    return rag(req, 'mistral').generate()
 
 
 @app.route(route="gpt35a")
 def route_gpt35_4k(req: func.HttpRequest) -> func.HttpResponse:
-    return model(req, 'gpt35_4k')
+    return rag(req, 'gpt35_4k').generate()
 
 
 @app.route(route="gpt35b")
 def route_gpt35_16k(req: func.HttpRequest) -> func.HttpResponse:
-    return model(req, 'gpt35_16k')
+    return rag(req, 'gpt35_16k').generate()
 
 
 @app.route(route="gpt4")
 def route_gpt4_1106(req: func.HttpRequest) -> func.HttpResponse:
-    return model(req, 'gpt4_1106')
+    return rag(req, 'gpt4_1106').generate()
 
 
-def model(req: func.HttpRequest, model: str) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    load_dotenv()
-    body = req.params.get('body')
-    use_rag = req.params.get('use_rag')
+class rag():
+    def __init__(self, req: func.HttpRequest, model: str):
+        load_dotenv()
+        self.body = req.params.get('body')
 
-    if not body:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
+        if not self.body:
+            try:
+                req = req.get_json()
+            except ValueError:
+                pass
+            else:
+                self.body = req.get('body')
+        
+        self.model = model
+        self.use_rag = bool(req.params.get('use_rag') or True)
+        self.temperature = float(req.params.get('temperature') or 0.9) 
+        self.top_p = float(req.params.get('top_p') or 0.9)
+        self.do_sample = bool(req.params.get('do_sample') or True) 
+        self.frequency_penalty = float(req.params.get('frequency_penalty') or 0) 
+        self.presence_penalty = float(req.params.get('presence_penalty') or 0) 
+        self.max_new_tokens = int(req.params.get('max_new_tokens') or 200) 
+        self.chunk_limit = int(req.params.get('chunk_limit') or 150) 
+        self.k = int(req.params.get('k') or 3) 
+
+        logging.info("Request parameters: %s", self.__dict__)
+
+        self.index = faiss.read_index('data/chunks.faiss')
+        self.data = pd.read_csv('data/data.csv')
+
+    def generate(self) -> func.HttpResponse:
+        if self.body:
+            embedding = self._embed()
+            scores, ids = self.index.search(embedding, k=self.k)
+            relevant_data = self.data.iloc[ids[0]]
+            context = ' | '.join(relevant_data['chunks'][0:self.chunk_limit])
+            self.prompt = f"Answer: {self.body} {('using: ' + context) if self.use_rag else ''}"
+
+            logging.info("Prompt: %s", self.prompt)
+
+            if self.model in ['llama', 'mistral']:
+                response = self._ml_studio_model()
+            elif self.model in ['gpt35_4k', 'gpt35_16k', 'gpt4_1106']:
+                response = self._ai_studio_model()
+
+            logging.info("Response: %s", response)
+
+            return func.HttpResponse(
+                json.dumps({
+                    "response": response,
+                    "source": relevant_data.assign(similarity=scores[0])[['title', 'similarity']].to_string(index=False),
+                    "context": list(relevant_data['chunks'])
+                }),
+                mimetype="application/json"
+            )
         else:
-            body = req_body.get('body')
-            use_rag = req_body.get('use_rag')
-
-    use_rag = True if use_rag in ['1', 'True', 'true', 1] else False
-
-    if body:
-        index = faiss.read_index('data/chunks.faiss')
-        data = pd.read_csv('data/data.csv')
-
-        embedding = requests.post(
-                        url = "https://ragnalysis.openai.azure.com/openai/deployments/ada_embedding/embeddings?api-version=2023-05-15", 
-                        headers = { "Content-Type": "application/json", "api-key": os.getenv('OPENAI_KEY') }, 
-                        json = { "input": body }
-                    ).json()['data'][0]['embedding']
-
-        embedding = np.array([embedding], dtype='float32')
-        scores, ids = index.search(embedding, k=3)
-
-        prompt = f"Answer: {body} {'' if not use_rag else ('using: ' + ' | '.join(data.iloc[ids[0]]['chunks'][0:150]))}"
-
-        print("Prompt: ", prompt)
-
-        response = ''
-
-        if model == 'llama':
-            response = ml_studio(
-                prompt = prompt,
-                model = model,
-                endpoint = 'jludq',
-                deployment = 'llama-2-7b-chat-18'
-            )
-        elif model == 'mistral':
-            response = ml_studio(
-                prompt = prompt,
-                model = model,
-                endpoint = 'ebbtk',
-                deployment = 'mistralai-mistral-7b-instruct-5'
-            )
-        elif model in ['gpt35_4k', 'gpt35_16k', 'gpt4_1106']:
-            response = ai_studio(
-                prompt = prompt,
-                model = model
+            return func.HttpResponse(
+                "This HTTP triggered function executed successfully. Pass a body in the query string or in the request body for a personalized response.",
+                status_code=200
             )
 
-        return func.HttpResponse(
-            json.dumps({
-                "response": response,
-                "source": data.iloc[ids[0]].assign(similarity=scores[0])[['title', 'similarity']].to_string(index=False),
-                "context": list(data.iloc[ids[0]]['chunks'])
-            }),
-            mimetype="application/json"
-        )
+    def _embed(self) -> list[float]:
+        try:
+            embedding = requests.post(
+                url = "https://ragnalysis.openai.azure.com/openai/deployments/ada_embedding/embeddings?api-version=2023-05-15", 
+                headers = { "Content-Type": "application/json", "api-key": os.getenv('OPENAI_KEY') }, 
+                json = { "input": self.body }
+            ).json()['data'][0]['embedding']
+        except KeyError as e:
+            raise e('ADA embedding API failed to embed: %s', self.body) 
+        else:
+            return np.array([embedding], dtype='float32')
 
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a body in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
+    def _ml_studio_model(self) -> str:
+        model = self.model.upper()
+        endpoint = os.getenv(f'{model}_ENDPOINT')
+        try:
+            response = requests.post(
+                url = f'https://ragnalysis-{endpoint}.eastus2.inference.ml.azure.com/score',
+                headers = {
+                    'Content-Type':'application/json',
+                    'Authorization':('Bearer '+ os.getenv(f'{model}_KEY')), 
+                    'azureml-model-deployment': os.getenv(f'{model}_MODEL')
+                },
+                json = {
+                    "input_data": {
+                        "input_string": [{
+                            "role": "user",
+                            "content": self.prompt
+                        }],
+                        "parameters": {
+                            "temperature": self.temperature,
+                            "top_p": self.top_p,
+                            "do_sample": self.do_sample,
+                            "max_new_tokens": self.max_new_tokens
+                        }
+                    }
+                }
+            ).json()
+            return response.get('output')
+        except KeyError as e:
+            raise e(f'ML studio model failed to generate prompt with the given context. \n \
+                    Try setting use_rag to False to see if it is an issue with the context. \n \
+                    Response object: {response}')
 
-
-def ml_studio(prompt: str, model: str, endpoint: str, deployment: str) -> str:
-    response = requests.post(
-        url = f'https://ragnalysis-{endpoint}.eastus2.inference.ml.azure.com/score',
-        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ os.getenv(f'{model.upper()}_KEY')), 'azureml-model-deployment': deployment },
-        json = {
-            "input_data": {
-                "input_string": [{
-                    "role": "user",
-                    "content": prompt
-                }],
-                "parameters": {
-                "temperature": 0.9,
-                "top_p": 0.9,
-                "do_sample": True,
-                "max_new_tokens": 200
-        }}}).json()
-
-    return response.get('output')
-
-
-def ai_studio(prompt: str, model: str) -> str:
-    response = requests.post(
-        url = f"https://ragnalysis.openai.azure.com/openai/deployments/{model}/chat/completions?api-version=2023-05-15", 
-        headers = { "Content-Type": "application/json", "api-key": os.getenv('OPENAI_KEY') }, 
-        json = { 
-            "messages": [{
-                "role": "user",
-                "content": prompt
-            }],
-            "temperature": 0.7,
-            "top_p": 0.95,
-            "frequency_penalty": 0,
-            "presence_penalty": 0,
-            "max_tokens": 200,
-            "stop": None
-        }
-    ).json()
-    
-    print("Response: ", response)
-    
-    return response['choices'][0]['message'].get('content')
+    def _ai_studio_model(self) -> str:
+        try:
+            response = requests.post(
+                url = f"https://ragnalysis.openai.azure.com/openai/deployments/{self.model}/chat/completions?api-version=2023-05-15", 
+                headers = { "Content-Type": "application/json", "api-key": os.getenv('OPENAI_KEY') }, 
+                json = { 
+                    "messages": [{
+                        "role": "user",
+                        "content": self.prompt
+                    }],
+                    "temperature": self.temperature,
+                    "top_p": self.top_p,
+                    "frequency_penalty": self.frequency_penalty,
+                    "presence_penalty": self.presence_penalty,
+                    "max_tokens": self.max_new_tokens,
+                    "stop": None
+                }
+            ).json()
+            return response['choices'][0]['message'].get('content')
+        except KeyError as e:
+            raise e(f'AI studio model failed to generate prompt with the given context. \n \
+                    Try setting use_rag to False to see if it is an issue with the context. \n \
+                    Response object: {response}')
