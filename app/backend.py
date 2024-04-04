e108a841b6a2cc4f5d47af8595d52ce1ee20aef7
Ubuntu <BLUOHC@BLUOHC.rzogni4mlg2utd2zwouju2c5qh.vx.internal.cloudapp.net>
2024-04-04 13:01:02 +0000

    Added docstrings

--- app/backend.py
+++ app/backend.py
@@ -19,7 +19,25 @@ EMBED_COST = 0.000136  # per 1000 tokens
 
 
 class rag():
-    def __init__(self, req: func.HttpRequest, model: str):
+    """Backend processor for the RAG engine"""
+
+    def __init__(self, req: func.HttpRequest, model: str) -> None:
+        """Extracting model parameters from the request object
+        
+        Args: 
+            body                : the prompt
+            model               : which model to use (ex gpt, llama, mistral)
+            use_rag             : whether to pass extra context or to just use the prompt
+            temperature         : randomness (higher = more)
+            top_p               : top % of most similar tokens to sample from
+            do_sample           : sample from a distribution of words, or just use the most likely word
+            frequency_penalty   : incremental penalty based on frequency of token use
+            presence_penalty    : flat penalty each time a repeated token is used
+            max_new_tokens      : max tokens to produce in the response
+            chunk_limit         : limit the size of each chunk passed to the llm (mostly for large chunk sizes)  
+            k                   : how many chunks to pass to the llm as context
+
+        """
         self.body = req.params.get('body')
 
         if not self.body:
@@ -32,23 +50,30 @@ class rag():
 
         self.model = model
         self.use_rag = req.params.get('use_rag') in ["True", True, None]
-        self.temperature = float(req.params.get('temperature') or 0.9) 
+        self.temperature = float(req.params.get('temperature') or 0.9)
         self.top_p = float(req.params.get('top_p') or 0.9)
         self.do_sample = req.params.get('do_sample') in ["True", True, None]
-        self.frequency_penalty = float(req.params.get('frequency_penalty') or 0) 
+        self.frequency_penalty = float(req.params.get('frequency_penalty') or 0)
         self.presence_penalty = float(req.params.get('presence_penalty') or 0) 
-        self.max_new_tokens = int(req.params.get('max_new_tokens') or 200) 
-        self.chunk_limit = int(req.params.get('chunk_limit') or 150) 
+        self.max_new_tokens = int(req.params.get('max_new_tokens') or 200)
+        self.chunk_limit = int(req.params.get('chunk_limit') or 150)
         self.k = int(req.params.get('k') or 3) 
 
     def generate(self, data, index) -> func.HttpResponse:
+        """Main method; coordinates other methods
+        
+        Args:
+            data    : pd.DataFrame containing all the chunks
+            index   : FAISS index containing embeddings for the chunks
+
+        """
         if self.body:
             embedding, embed_time = self._embed()
             (scores, ids), search_time = self._index(index, embedding)
             entity, entity_extraction_time = self._entity_extractor()
-            relevant_data = data.iloc[ids[0]]
-            context = ' | '.join(relevant_data['chunks'][0:self.chunk_limit])
-            self.prompt = f"Answer: {self.body} {('using: ' + context) if self.use_rag else ''}"
+            relevant_data = data.iloc[ids[0]]  # the most relevant chunks
+            context = ' | '.join(relevant_data['chunks'][0:self.chunk_limit])  # joining the relevant chunks into a single string
+            self.prompt = f"Answer: {self.body} {('using: ' + context) if self.use_rag else ''}"  # adding the chunks to the prompt
             response, generate_time = self._augment()
 
             logging.info("Response: %s", response)
@@ -102,7 +127,14 @@ class rag():
             )
 
     @timer
-    def _embed(self) -> list[float]:
+    def _embed(self) -> tuple[list[float], float]:
+        """Embedding the user prompt into a numeric vector representation for processing
+        
+        Returns:
+            list[float] : an ada002 embedding of size 1536
+            float       : function runtime in seconds 
+        
+        """
         try:
             embedding = requests.post(
                 url = "https://ragnalysis.openai.azure.com/openai/deployments/ada_embedding/embeddings?api-version=2023-05-15", 
@@ -115,12 +147,28 @@ class rag():
             return np.array([embedding], dtype='float32')
 
     @timer
-    def _index(self, index, embedding) -> list:
+    def _index(self, index, embedding) -> tuple[list[float], list[int], float]:
+        """Searches the FAISS index using the prompt embedding for the most similar chunk embeddings 
+        
+        Returns:
+            list[float]     : similarity scores of the retrieved chunks in the range [0, 1]
+            list[int]       : indexes of the most relevant chunks in the pd.DataFrame containing the chunks in string form
+            float           : function runtime in seconds
+
+        """
         scores, ids = index.search(embedding, k=self.k)
         return scores, ids
 
     @timer
-    def _augment(self) -> str:
+    def _entity_extractor(self) -> tuple[str, float]:
+        """TODO: Entity extraction function for extracting entities from user prompts"""
+
+        return ""
+
+    @timer
+    def _augment(self) -> tuple[str, float]:
+        """Parent function for choosing which LLM to prompt for a response"""
+
         if self.model in ['llama', 'mistral']:
             response = self._ml_studio_model()
         elif self.model in ['gpt35_4k', 'gpt35_16k', 'gpt4_1106']:
@@ -131,11 +179,9 @@ class rag():
             raise Exception("Invalid model choice")
         return response
 
-    @timer
-    def _entity_extractor(self) -> str:
-        return ""
-
     def _ml_studio_model(self) -> str:
+        """Child function (1/3) of _augment() that routes to the ML Studio models"""
+
         model = self.model.upper()
         endpoint = os.environ[f'{model}_ENDPOINT']
         try:
@@ -168,6 +214,7 @@ class rag():
                     Response object: {response}')
 
     def _ai_studio_model(self) -> str:
+        """Child function (2/3) of _augment() that routes to the OpenAI Studio models"""
         try:
             response = requests.post(
                 url = f"https://ragnalysis.openai.azure.com/openai/deployments/{self.model}/chat/completions?api-version=2023-05-15", 
@@ -192,6 +239,7 @@ class rag():
                     Response object: {response}')
 
     def _containerized_model(self) -> str:
+        """Child function (3/3) of _augment() that routes to the containerized models"""
         try:
             return requests.post(
                 url="https://localai-selfhost.salmonground-3deb4a95.canadaeast.azurecontainerapps.io/chat/completions",
