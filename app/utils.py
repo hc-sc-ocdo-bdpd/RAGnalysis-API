041cd6db8735326feb2558cdb5310801371a7bb2
Ubuntu <BLUOHC@BLUOHC.rzogni4mlg2utd2zwouju2c5qh.vx.internal.cloudapp.net>
2024-04-04 13:09:22 +0000

    Added docstrings

--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,13 @@ from azure.storage.blob import BlobClient
 
 
 def read_blob(blob_name: str, operation):
+    """Loads from Azure blob storage to a local variable
+    
+    Args:
+        blob_name : Name of the file on blob storage
+        operation: The function to perform on the blob (ex pd.read_csv, faiss.read_index)
+
+    """
     blob = BlobClient(
                 account_url=os.environ['STORAGE_URL'], 
                 container_name=os.environ['STORAGE_CONTAINER'], 
@@ -19,7 +26,9 @@ def read_blob(blob_name: str, operation):
         return operation(temp_file.name)
 
 
-def timer(some_function):
+def timer(some_function) -> float:
+    """Decorator for timing function runtime in seconds"""
+
     def wrapper(*args, **kwargs):
         t1 = time()
         result = some_function(*args, **kwargs)
@@ -29,6 +38,12 @@ def timer(some_function):
 
 
 def count_tokens(text: str, model_name: str) -> int:
+    """Count the number of tokens in a string
+    
+    Args:
+        model_name: The model used to count tokens (ex. gpt-3.5-turbo, text-embedding-ada-002)
+    
+    """
     encoding = tiktoken.encoding_for_model(model_name)
     ntokens = len(encoding.encode(text))
     return ntokens
