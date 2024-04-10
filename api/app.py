d276ab654288271ae3ef131e95efba080c8d4db2
Ubuntu <BLUOHC@BLUOHC.rzogni4mlg2utd2zwouju2c5qh.vx.internal.cloudapp.net>
2024-04-10 18:17:26 +0000

    Updated api code

--- api/app.py
+++ api/app.py
@@ -1,15 +1,14 @@
 import os
 import requests
 from typing import Optional
-from dotenv import load_dotenv
-
-load_dotenv()
+import pandas as pd
 
 class RagnalysisClient:
-    def __init__(self, api_key: Optional[str] = None):
+    def __init__(self, api_key: Optional[str] = None, persist: bool = False):
         self.api_key = api_key if api_key else os.getenv('FUNCTION_KEY')
         self.models = ['llama', 'mistral', 'gpt3s', 'gpt3l', 'gpt4', 'qwen']
         self.base_url = f'https://med-app.azurewebsites.net/api'
+        self.persist = persist
 
         if not self.api_key:
             raise ValueError("API key is not provided or set in environment variables.")
@@ -23,22 +22,40 @@ class RagnalysisClient:
                presence_penalty: Optional[float] = 0, max_new_tokens: Optional[int] = 200,
                chunk_limit: Optional[int] = 150, k: Optional[int] = 3) -> dict[str: any]:
         
-        response = requests.post(
-            url = f"{self.base_url}/{model}?code={self.api_key}", 
-            params = {
-                "body": prompt, 
-                "use_rag": use_rag,
-                "temperature": temperature,
-                "top_p": top_p,
-                "do_sample": do_sample,
-                "frequency_penalty": frequency_penalty,
-                "presence_penalty": presence_penalty,
-                "max_new_tokens": max_new_tokens,
-                "chunk_limit": chunk_limit,
-                "k": k
-            }
-        )
-        return response.json()        
+        try:
+            response = requests.post(
+                url = f"{self.base_url}/{model}?code={self.api_key}", 
+                params = {
+                    "body": prompt, 
+                    "use_rag": use_rag,
+                    "temperature": temperature,
+                    "top_p": top_p,
+                    "do_sample": do_sample,
+                    "frequency_penalty": frequency_penalty,
+                    "presence_penalty": presence_penalty,
+                    "max_new_tokens": max_new_tokens,
+                    "chunk_limit": chunk_limit,
+                    "k": k
+                }
+            ).json()
+        except Exception as e:
+            raise e("ERROR: API failed to process")
+        else:
+            if self.persist: 
+                self._persist(response)
+            return response
+    
+    def _persist(self, response):
+        '''Writes the response object to a lakehouse table'''
+        from pyspark.sql import SparkSession
+
+        df = pd.json_normalize(response, sep="_")
+        df['titles'], df['chunks'], df['urls'], df['similarities'] = [str([value[key] for value in df['sources'][0]]) for key in ['title', 'chunks', 'url', 'similarity']]
+        df = df.drop(['sources'], axis=1)
+
+        spark = SparkSession.builder.getOrCreate()
+        spark_df = spark.createDataFrame(df)
+        spark_df.write.mode("append").format("delta").saveAsTable("responses")
 
     def llama(self, prompt: str, **kwargs):
         return self._model(prompt, model='llama', **kwargs)
